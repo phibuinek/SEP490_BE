d6db9bc07a9176a94bae661abc877672fdf66169
briantran2003 <bao.tranlechi05@gmail.com>
2025-07-17 00:39:08 +0700

    fix API GET BedAssignments

--- src/bed-assignments/bed-assignments.module.ts
+++ src/bed-assignments/bed-assignments.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import {
   BedAssignment,
@@ -6,12 +6,14 @@ import {
 } from './schemas/bed-assignment.schema';
 import { BedAssignmentsController } from './bed-assignments.controller';
 import { BedAssignmentsService } from './bed-assignments.service';
+import { ResidentsModule } from '../residents/residents.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: BedAssignment.name, schema: BedAssignmentSchema },
     ]),
+    forwardRef(() => ResidentsModule),
   ],
   controllers: [BedAssignmentsController],
   providers: [BedAssignmentsService],
