3ce48dd09be930b66155e01cf9fe72f9e6b34a92
phibuinek <phibuinek@gmail.com>
2025-09-02 16:57:40 +0700

    fix bug

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import * as crypto from 'crypto-js';
 import * as moment from 'moment';
-import { CarePlansService } from 'src/care-plans/care-plans.service';
+import { CarePlansService } from '../care-plans/care-plans.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
